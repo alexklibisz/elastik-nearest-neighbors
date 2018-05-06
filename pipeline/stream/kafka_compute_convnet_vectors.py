8704533ec4359a7b8131d5a6459000e498402fb6
Ubuntu <ubuntu@ip-172-31-29-177.ec2.internal>
2018-05-06 17:44:06 +0000

    PIPELINE - optimized convnet computation step for use with S3

--- pipeline/stream/kafka_compute_convnet_vectors.py
+++ pipeline/stream/kafka_compute_convnet_vectors.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+from concurrent.futures import ThreadPoolExecutor
 from kafka import KafkaConsumer, KafkaProducer
 from io import BytesIO
 from multiprocessing import Pool, cpu_count
@@ -6,8 +7,11 @@ from scipy.misc import imread, imsave
 from lycon import resize
 from time import time
 from tqdm import tqdm
+import boto3
+import json
 import numpy as np
 import pdb
+import sys
 
 from keras.models import Model
 from keras.applications import MobileNet
@@ -32,9 +36,9 @@ class Convnet(object):
 
         return labels, vecs
 
-def _msg_to_img(msg):
+def _preprocess_img(img_bytes):
     # Read from bytes to numpy array.
-    img = imread(BytesIO(msg.value))
+    img = imread(img_bytes)
 
     # Extremely fast resize.
     img = resize(img, 224, 224, interpolation=0)
@@ -50,12 +54,17 @@ def _msg_to_img(msg):
     # Other image: repeat first channel 3 times.
     return np.repeat(img[:, :, :1], 3, -1)
 
+def _get_img_bytes_from_s3(args):
+    bucket, key, s3_client = args
+    obj = s3_client.get_object(Bucket=bucket, Key=key)
+    return BytesIO(obj['Body'].read())
+
 if __name__ == "__main__":
 
     ap = ArgumentParser(description="See script header")
     ap.add_argument("--kafka_sub_topic", 
                     help="Name of topic from which images are consumed", 
-                    default="aknn-demo.images")
+                    default="aknn-demo.image-objects")
     ap.add_argument("--kafka_pub_topic", 
                     help="Name of topic to which feature vectors get produced", 
                     default="aknn-demo.convnet-features")
@@ -66,7 +75,6 @@ if __name__ == "__main__":
                     help="Group ID for Kafka consumer", 
                     default="aknn-demo.comput-convnet-features")
     ap.add_argument("-b", "--batch_size", type=int, default=100)
-    ap.add_argument("-w", "--max_wait", type=int, default=60)
 
     args = vars(ap.parse_args())
 
@@ -76,26 +84,40 @@ if __name__ == "__main__":
         group_id=args["kafka_group"],
         auto_offset_reset="earliest",
         key_deserializer=lambda k: k.decode(),
+        value_deserializer=lambda v: json.loads(v.decode())
     )
     
+    s3_client = boto3.client('s3')
+
 #    producer = KafkaProducer(bootstrap_servers=",".join(KAFKA_SERVERS))
     convnet = Convnet()
     pool = Pool(cpu_count())
-    last_batch_t = time()
-    batch = []
+    tpex = ThreadPoolExecutor(max_workers=12)
 
     for msg in consumer:
     
-        batch.append(msg)
+        print("%d images in batch" % len(msg.value))
+        T0 = time()
 
-        if len(batch) < args["batch_size"] and (time() - last_batch_t) < args["max_wait"]:
-            continue
+        t0 = time()
+        data = map(lambda o: (o['bucket'], o['key'], s3_client), msg.value)
+        imgs_bytes = list(tpex.map(_get_img_bytes_from_s3, data))
+        print("Download images", time() - t0)
+        
+        t0 = time()
+        imgs_iter = pool.map(_preprocess_img, imgs_bytes)
+        print("Preprocess images", time() - t0)
 
-        imgs_iter = pool.map(_msg_to_img, batch)
+        t0 = time()
         labels, vecs = convnet.get_labels_and_vecs(imgs_iter)
+        print("Compute", time() - t0)
         
         print("%s: %s %.2lf, %.2lf, %d" \
-            % (msg.key, str(vecs.shape), vecs.mean(), vecs.std(), time() - last_batch_t))
-        last_batch_t = time()
-        batch = []
+            % (msg.key, str(vecs.shape), vecs.mean(), vecs.std(), time() - T0))
+
+        #np.save("/home/ubuntu/tmp/%s.npy" % msg.key.split('.')[0], vecs.astype(np.float16))
+        #with open("/home/ubuntu/tmp/%s.txt" % msg.key.split('.')[0],"w") as fp:
+        #    fp.write("\n".join([m.key for m in batch]))
+        
+        #batch = []
 
