46c2db8c037211c82c6ebf7e0615c72740e3b743
Alex Klibisz <aklibisz@gmail.com>
2018-05-16 11:40:38 -0400

    ES - added trivial unit and integration tests

---
+++ elasticsearch-aknn/src/test/java/org/elasticsearch/plugin/aknn/AknnSimpleTests.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright [2018] [Alex Klibisz]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.elasticsearch.plugin.aknn;
+
+import org.elasticsearch.test.ESTestCase;
+
+public class AknnSimpleTests extends ESTestCase {
+
+    // Note: tests must start with the word "test"...
+
+    public void testThatAnythingWorks() {
+//        System.out.println(logger.getLevel().toString());
+//        logger.debug("Testing logger config....");
+//        logger.debug("Testing logger config....");
+//        logger.debug("Testing logger config....");
+//        logger.debug("Testing logger config....");
+//        logger.debug("Testing logger config....");
+//        logger.debug("Testing logger config....");
+        long i = 99L;
+        assertEquals(i, 99L);
+    }
+
+    public void testThatSomethingElseWorks() {
+        long i = 98L;
+        assertEquals(i, 98L);
+    }
+
+    public void testSomethingWorks() {
+        long i = 97L;
+        assertEquals(i, 97L);
+    }
+
+    public void testSomethingElse() {
+        long i = 96L;
+        assertEquals(i, 96L);
+    }
+
+
+
+}
